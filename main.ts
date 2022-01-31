9259e890fecdddfa935f5b843963fb1025b410f2
Scott Carpenter <48895574+scotttech17@users.noreply.github.com>
2022-01-31 10:43:37 -0500

    First Program, made music note logo and plays joyful joyful on start

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+basic.showLeds(`
+    . . # # #
+    . . # . .
+    . . # . .
+    . # # . .
+    . # # . .
+    `)
+music.playMelody("E E F G G F E D ", 200)
+music.playMelody("C C D E E D D - ", 200)
+basic.forever(function () {
+	
+})
